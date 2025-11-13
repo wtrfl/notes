67325cb292068f35b257640726297cc53e16fe99
waterfall <78034265+wtrfl@users.noreply.github.com>
2025-11-12 20:45:47 -0700

    add documents to firestore

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+'use client';
+
+import { db } from '@/lib/firebase';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 
 export default function Home() {
+
+    const addNote = async () => {
+        const notesRef = collection(db, "notes");
+
+        await addDoc(notesRef, {
+            title: "Note",
+            content: "test",
+            createdAt: serverTimestamp()
+        });
+    }
+
     return (
         <div className="flex min-h-screen justify-center items-center">
-            Hi
+            Hi<button onClick={addNote}>Add</button>
         </div>
     );
 }
