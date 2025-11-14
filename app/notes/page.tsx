56ae6eb776f63320f32bd2ed697dd8a034c1b737
waterfall <78034265+wtrfl@users.noreply.github.com>
2025-11-13 21:35:00 -0700

    feat: select other notes

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -76,7 +76,7 @@ export default function Notes() {
                         New Note
                     </button>
                     {notes.map(note => (
-                        <div className={"border-b flex flex-col px-4 py-3 bg-foreground/(--bg-opacity) " + (note.id === currentNoteId ? "[--bg-opacity:10%]" : "[--bg-opacity:0%]")} key={note.id}>
+                        <div onClick={() => setCurrentNoteId(note.id)} className={"border-b flex flex-col px-4 py-3 cursor-pointer bg-foreground/(--bg-opacity) " + (note.id === currentNoteId ? "[--bg-opacity:10%]" : "[--bg-opacity:0%] hover:[--bg-opacity:5%]")} key={note.id}>
                             <strong>{note.title}</strong>
                             <span>
                                 {formattedNoteDate(note.createdAt, "short")}
