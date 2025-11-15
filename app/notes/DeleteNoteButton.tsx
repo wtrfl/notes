dcbc4b34b77b877faf049804cb4b2d39412a0044
waterfall <78034265+wtrfl@users.noreply.github.com>
2025-11-14 18:40:42 -0700

    style: change red color so grayscale is more consistent

--- app/notes/DeleteNoteButton.tsx
+++ app/notes/DeleteNoteButton.tsx
@@ -14,7 +14,7 @@ export default function DeleteNoteButton({ id, user, setCurrentNoteId }: { id: s
 
     return (
         <div className="absolute top-0 right-0 my-2.5 mx-4.5 cursor-pointer grayscale hover:grayscale-0" onClick={handleDelete}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="red" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-trash-icon lucide-trash"><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ff7373" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-trash-icon lucide-trash"><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
         </div>
     )
 }
