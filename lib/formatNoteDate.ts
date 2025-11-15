ad1435df4935237f828b6a38e413514cc84df5bc
waterfall <78034265+wtrfl@users.noreply.github.com>
2025-11-14 18:51:59 -0700

    fix: date formatter to always show a date

--- lib/formatNoteDate.ts
+++ lib/formatNoteDate.ts
@@ -5,28 +5,40 @@ const monthNames = [
   "July", "August", "September", "October", "November", "December"
 ];
 
+const shortDate = (date: Date) => (
+    date.getMonth()+1 + "/" +
+    date.getDate() + "/" +
+    date.getFullYear().toString().slice(-2)
+)
+
+const longDate = (date: Date) => {
+    const hours = date.getHours();
+    const formattedHours =  hours > 12 ? hours-12 : hours;
+    const ampm = hours > 12 ? 'PM' : 'AM';
+    return (
+        monthNames[date.getMonth()] + " " +
+        date.getDate() + ", " +
+        date.getFullYear() + " at " +
+        formattedHours + ":" +
+        date.getMinutes() + " " +
+        ampm
+    )
+}
+
 export default function formattedNoteDate(timestamp: Timestamp, style: "short" | "long") {
-    if (!timestamp) return "";
-    
-    const lastModified = timestamp.toDate();
+    let date;
+
+    if (timestamp) {
+        date = timestamp.toDate();
+    } else {
+        date = new Date();
+    }
 
     if (style === "short") {
-        return (
-            lastModified.getMonth()+1 + "/" +
-            lastModified.getDate() + "/" +
-            lastModified.getFullYear().toString().slice(-2)
-        )
-    } else if (style === "long") {
-        const hours = lastModified.getHours();
-        const formattedHours =  hours > 12 ? hours-12 : hours;
-        const ampm = hours > 12 ? 'PM' : 'AM';
-        return (
-            monthNames[lastModified.getMonth()] + " " +
-            lastModified.getDate() + ", " +
-            lastModified.getFullYear() + " at " +
-            formattedHours + ":" +
-            lastModified.getMinutes() + " " +
-            ampm
-        )
+        return shortDate(date);
+    } else if (style === "long" && timestamp) {
+        return longDate(date)
+    } else if (style === "long" && !timestamp) {
+        return "fetching..."
     }
 }
